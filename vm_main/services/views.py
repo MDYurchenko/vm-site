585607ba9c5617ced646305b3ca2aad8e6d6cade
Максим Юрченко <migismigis@gmail.com>
2024-07-25 23:39:45 +0700

    fix views functionality, work with djangoORM, create some endpoint

--- vm_main/services/views.py
+++ vm_main/services/views.py
@@ -10,13 +10,15 @@ def hello_world(request):
 def get_contract_short_info(request):
     simple_answer = {
         'status': 200,
-        'answer':'it is good'
+        'answer': 'it is good'
     }
     return JsonResponse(simple_answer)
 
+
 def get_contracts_list(request):
-    contracts = Contract.objects.all() #models.Manager???
+    contracts = Contract.objects.all()# models.Manager???
+    d = [{'number': entry.number, 'client': entry.client.organization_name, 'service': entry.service.name} for entry in contracts]
     data = {
-        'contracts': [contracts]
+        'contracts': [d]
     }
     return JsonResponse(data)
